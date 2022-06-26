bb8edf35281047d8b77d7f9df9ab9b65af84c630
thedesalizes <altaria99@gmail.com>
2022-06-26 13:31:33 +0800

    Updated createevent command in Slash.py

--- discord/cogs/Slash.py
+++ discord/cogs/Slash.py
@@ -1,8 +1,7 @@
-import xdrlib
 import disnake
 from disnake.ext import commands
 from typing import List
-from datetime import date
+from datetime import datetime
 import sys
 sys.path.append('..')
 from connection import supabaseinteraction
@@ -26,15 +25,30 @@ class Slash(commands.Cog):
     async def myid(ctx):
         await ctx.response.send_message(str(ctx.author.id))
 
-    @commands.slash_command(description="Create an Event, specify date as DDMMYY, and duration in hours")
-    async def createevent(ctx, activity: str, day: str, duration: int = 0):
-        event = {
-            "activity": activity,
-            "duration": duration,
-            "discgrp": ctx.channel_id
-        }
-        await supabaseinteraction.send_event(event)
-        await ctx.response.send_message(str(ctx.author.id))
+    @commands.slash_command(description="Create an Event, specify date as DDMMYY, time in HHMM, and duration in hours")
+    async def createevent(ctx, activity: str, date: str, time: str, duration: int = 0):
+        if(len(date) != 6 or len(time) != 4):
+            await ctx.response.send_message("Invalid Date/Time!")
+            return
+        day = int(date[0:2])
+        month = int(date[2:4])
+        year = int(date[4:]) + 2000
+        hour = int(time[0:2]) - 8
+        minute = int(time[2:])
+        try:
+            ts = datetime(year, month, day, hour, minute).isoformat() 
+            event = {
+                "activity": activity,
+                "starttime": ts,
+                "duration": duration,
+                "discgrp": ctx.channel_id
+            }
+            await supabaseinteraction.send_event(event)
+            await ctx.response.send_message(str(ctx.author.id))
+        except ValueError:
+            await ctx.response.send_message("Invalid Date/Time!")
+
+        
 
     @commands.slash_command(description="Edit an event")
     async def editevent(ctx, action: str, activty: str, x):
